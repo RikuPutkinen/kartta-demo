11a6ea3d77e3714168d3973315f22b5a17b99a14
Riku Putkinen <putkinenriku@gmail.com>
2024-04-19 11:01:50 +0300

    Add image carousel

--- client/src/components/cameraMarker.jsx
+++ client/src/components/cameraMarker.jsx
@@ -2,10 +2,11 @@ import { Marker, Popup, useMapEvents } from 'react-leaflet'
 import { CameraIcon } from '../utils/icons'
 import { useState } from 'react'
 import { useWeatherCamera } from '../hooks/weatherCameraHooks'
+import { Carousel } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function CameraContainer({ id }) {
   const res = useWeatherCamera(id)
-  console.log('fetching...')
 
   if (res.isLoading) {
     return <div>Loading...</div>
@@ -13,8 +14,31 @@ function CameraContainer({ id }) {
   if (!res.isSuccess) {
     return <div>Error :(</div>
   }
-  console.log(res.data)
-  return <div>Success :)</div>
+
+  const { dataUpdatedTime, names, presets } = res.data.properties
+  console.log(presets)
+  console.log(dataUpdatedTime)
+  const lastUpdated = new Date(dataUpdatedTime)
+  return (
+    <div>
+      <Carousel slide={false} indicators={false} interval={null}>
+        {presets.map(p => {
+          return (
+            <Carousel.Item key={p.id}>
+              <div>
+                <img src={p.imageUrl} />
+              </div>
+              <Carousel.Caption>
+                <h4>{p.presentationName}</h4>
+              </Carousel.Caption>
+            </Carousel.Item>
+          )
+        })}
+      </Carousel>
+      <h3 className="text-base font-bold">{names.en}</h3>
+      <p>Last updated: {lastUpdated.toString()}</p>
+    </div>
+  )
 }
 
 export default function CameraMarker({ data }) {
@@ -30,7 +54,9 @@ export default function CameraMarker({ data }) {
         popupclose: () => setIsOpen(false),
       }}
     >
-      <Popup>{isOpen && <CameraContainer id={data.id} />}</Popup>
+      <Popup>
+        <div className="w-64">{isOpen && <CameraContainer id={data.id} />}</div>
+      </Popup>
     </Marker>
   )
 }
